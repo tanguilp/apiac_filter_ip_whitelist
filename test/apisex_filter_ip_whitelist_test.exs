2017e62f644970c858e2c7ea05c75926bf41a749
Tangui <29804907+tanguilp@users.noreply.github.com>
2018-11-07 22:11:06 +0300

    Added tests

--- test/apisex_filter_ip_whitelist_test.exs
+++ test/apisex_filter_ip_whitelist_test.exs
@@ -27,6 +27,72 @@ defmodule APISexFilterIPWhitelistTest do
     assert conn.halted
   end
 
+  test "valid IPv6 address" do
+    opts = APISexFilterIPWhitelist.init(whitelist: ["2001:F4E5:C0CA:4000::/50"])
+
+    conn =
+      conn(:get, "/")
+      |> put_ip_address("2001:F4E5:C0CA:4049:D7:912E:FF00:0BD7")
+      |> APISexFilterIPWhitelist.call(opts)
+
+    refute conn.status == 403
+    refute conn.halted
+  end
+
+  test "invalid IPv6 address" do
+    opts = APISexFilterIPWhitelist.init(whitelist: ["2001:F4E5:C0CA:4000::/50"])
+
+    conn =
+      conn(:get, "/")
+      |> put_ip_address("2001:F4E5:C0CA:E049:D7:912E:FF00:0BD7")
+      |> APISexFilterIPWhitelist.call(opts)
+
+    assert conn.status == 403
+    assert conn.halted
+  end
+
+  test "subnet list with valid address" do
+    whitelist = [
+      "192.168.13.0/24",
+      "2001:45B8:991A::/48",
+      "23.12.0.0/16",
+      "20E7:4128:D4F0:0::/64",
+      "91.23.251.0/24"
+    ]
+    opts = APISexFilterIPWhitelist.init(whitelist: whitelist)
+
+    conn =
+      conn(:get, "/")
+      |> put_ip_address("20E7:4128:D4F0:0::42")
+      |> APISexFilterIPWhitelist.call(opts)
+
+    refute conn.status == 403
+    refute conn.halted
+  end
+
+  defp put_ip_address(conn, ip_address) do
+    %{conn | remote_ip: InetCidr.parse_address!(ip_address)}
+  end
+
+  test "subnet list with invalid address" do
+    whitelist = [
+      "192.168.13.0/24",
+      "2001:45B8:991A::/48",
+      "23.12.0.0/16",
+      "20E7:4128:D4F0:0::/64",
+      "91.23.251.0/24"
+    ]
+    opts = APISexFilterIPWhitelist.init(whitelist: whitelist)
+
+    conn =
+      conn(:get, "/")
+      |> put_ip_address("8.8.7.8")
+      |> APISexFilterIPWhitelist.call(opts)
+
+    assert conn.status == 403
+    assert conn.halted
+  end
+
   defp put_ip_address(conn, ip_address) do
     %{conn | remote_ip: InetCidr.parse_address!(ip_address)}
   end
